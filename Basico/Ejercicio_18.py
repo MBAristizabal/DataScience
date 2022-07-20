7fb3fe2b79c2b0c2e163bde4ef34a024d19cf714
MBAristizabal <mbaristizabal@hotmail.com>
2022-07-20 14:18:48 +0200

    ejercicio

---
+++ Basico/Ejercicio_18.py
@@ -0,0 +1,16 @@
+"""
+1)- Definir una función max() que tome como argumento dos números y devuelva el mayor de ellos.
+(Es cierto que python tiene una
+función max() incorporada, pero hacerla nosotros mismos es un muy buen ejercicio.
+"""
+def maximo(x,y):
+    if x > y :
+        return x
+        print(f'el valor {y} es el valor maximo')
+    else:
+        print(f'el valor {y} es el valor maximo')
+    
+maximo (2, 5)
+
+maximo (10, 8)
+
