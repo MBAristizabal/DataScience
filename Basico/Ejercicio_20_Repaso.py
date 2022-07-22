453656d6e91ff774494ecc5186249425bd4f8370
MBAristizabal <mbaristizabal@hotmail.com>
2022-07-22 20:53:36 +0200

    ejercicio

---
+++ Basico/Ejercicio_20_Repaso.py
@@ -0,0 +1,18 @@
+# Ejercicios
+#
+# *1)** Definir una función generar_n_caracteres() que tome un entero n y devuelva el caracter multiplicado por n. Por ejemplo: generar_n_caracteres(5, "x") debería devolver "xxxxx".
+
+
+#**2)** Definir un diagrama procedimiento() que tome una lista de números enteros e imprima un diagrama en la pantalla. Ejemplo: procedimiento([4, 9, 7]) debería imprimir lo siguiente:
+
+"****"
+
+"*********"
+
+"*******"
+
+
+#**3)** Escribir una función mas_larga() que tome una lista de palabras y devuelva la mas larga.
+
+#**4)** Escribir una función filtrar_palabras() que tome una lista de palabras y un entero n, y devuelva las palabras que tengan mas de n caracteres.
+
