4e196767d56c056455a63a132395834967b3afe0
MBAristizabal <mbaristizabal@hotmail.com>
2022-09-01 11:51:04 +0200

    practica

--- Basico/Ejercicio_1.py
+++ Basico/Ejercicio_1.py
@@ -11,7 +11,7 @@ print(notas)
 """
 Imprime los valores de "s" es igual a 25, de "y" es igual a 10, poniendo la siguiente salida:"""
 """El valor de "s" es "valor de s" y el valor de "y" es "valor de y"
-""""
+"""
 s = 25
 y = 10
 print(f'el valor de s es {s} y el valor de y es {y}')
@@ -29,13 +29,14 @@ name = "Juan 'El Profesor'"
 name 
 
 
+
 # EJERCICIO 4
 
 """
     Concatena las siguientes palabras formando un sola:
     Juan "El profesor"
 """
-name= " Juan" + " "+ " El Profesor"
+name= " Juan" + " " + " El Profesor"
 name
 
 # EJERCICIO 5
@@ -46,6 +47,19 @@ name
      · Pon todas las letras en mayúsculas
      · Pon todas las letras en minúculas
 """
+
+frase= "No cuentes los dias, haz que los dias cuenten"
+
+frase = frase.title()
+frase
+
+frase = frase.upper()
+frase
+
+frase = frase.lower()
+frase
+
+
 
 # EJERCICIO 6
 
@@ -58,12 +72,42 @@ name
     Muestra de que tipo se trata
 """
 
+suma = 26 + 35
+suma
+
+multi= 26 * 35
+multi
+
+operacion = (2+32)*10
+operacion 
+
+elevado = 3**9
+elevado
+
+redondea = round(elevado, 2)
+redondea
+
+type(redondea)
+
 # EJERCICIO 7
 
 """
     Saca el valor absoluto de -32
     Muestra el máximo y el mínimo de (3, -6, 4, -10, 2.6666)
 """
+
+absoluto = abs(-32)
+absoluto
+
+listado = [3,-6,4,-10, 2.6666]
+listado
+
+min(listado)
+
+max(listado)
+
+
+
 
 # EJERCICIO 8
 
@@ -73,3 +117,4 @@ name
     1) Hazlo de la forma más fácil posible teniendo en cuenta la posición (index) de esos valores.
     2) Crea un dataframe con esos valores (L = [10, None, 8, 5, None, 20])
 """
+
